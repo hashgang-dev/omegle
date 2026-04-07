c0a32b5ea4315cbc25baf4316bac0b0f874e817d
hashgang-dev <ramohan.chinta@gmail.com>
2026-04-07 09:00:52 +0530

    Change

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ async function start() {
     config: {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:global.stun.twilio.com:3478" },
+
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
       ],
     },
   });
